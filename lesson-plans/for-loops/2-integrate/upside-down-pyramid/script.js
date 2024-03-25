c72b4464f2e3203c606749806304398f1b2e2d3d
Etefa G.D <etefag@gmail.com>
2024-03-25 21:33:39 +0100

    add 'for loop-skippedString'

--- lesson-plans/for-loops/2-integrate/upside-down-pyramid/script.js
+++ lesson-plans/for-loops/2-integrate/upside-down-pyramid/script.js
@@ -1,15 +1,29 @@
 import { readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById(buttonID).addEventListener('click', () => {
   debugger;
 
   // read user text
+const userInput = readStrig('text');
 
   // create the pyramid
-
-  for (_; _; _) {
-    for (_; _; _) {}
+const pyramidHeight = 7;
+let pyramid = '';
+  for (i = 1; i <= pyramidHeight; i++) {
+    for (let j = 1; j <= pyramidHeight - i; j++) {
+      pyramid += ' ';
   }
 
+?
+?
+?
+?
+?
+
+
+
+
+
   // display the pyramid
+  display(originalString);
 });
